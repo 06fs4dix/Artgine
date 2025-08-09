d4856f36bcbc104fefe95844d5ff2b785d5ec29a
06fs4dix <41887436+06fs4dix@users.noreply.github.com>
2025-08-09 18:25:17 +0900

    팔레트 깃허브 페이지 적용

--- artgine/util/CPalette.js
+++ artgine/util/CPalette.js
@@ -8,6 +8,13 @@ import { CUtilRender } from "../render/CUtilRender.js";
 import { CUtil } from "../basic/CUtil.js";
 import { CLoaderOption } from "./CLoader.js";
 var gNoneImg = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAACXBIWXMAAAsSAAALEgHS3X78AAAGbklEQVRoge2aUUgUXRTH/2MgBULN0sKWsdUkGIIgzEdgCIWNQSBFwkgERU+zYAVR0KyvZTRbFIL0MEORBAviQBEJS7jWgikVuwhaYcguFIWQ5CqZkmjnexg/Xded2ZlZ/UTw97Z3zr3n/u/ce+69Z5YhgCGAISJsIBgGIIYYMCAQNlTfM2DAFIHZqL0HAIY22tRZQdF6d6BQNgWsN5sC1ptNAWvK9++4f9/aZIWA+Xm0t69Rf5wxNoZjx1BSkseMspiYIJ4nScou/5/59YsOHaLbt/MarhBgVK6pobNnaW5u9Xtmhz9/qLaWrl61Y2tylJiZQX09PB6EwyguXosJYsr8PM6cQXExwmE75iaLeNs2RCKYncWpU5iZWc3+5aWpCbOzePLEprl5FCouhq7D48GJE5iaWp3O5aW5GZ8+oaMDW7bYrZJnis3N0YULVF1NExOFz+083LtHlZVOHeUTYHDpElVV0Y8fbrplk3CYyspodNRpPXsCiCgYpIoKFw5s8fw5+f00MuKiqm0BRNTSQvv305cvLtxY0dtLPh8NDrqr7UQAEbW2uh6q3AwOks9H/f2uG3AogIhUlUpL6eNH1y6XGBkhv5+ePy+kDecCiCgcJq+X4vFCHNPoKJWVUThcUCMuBRDR06fk9bp/9RMTVFlJra0uq2fgVgARRSLk8VBPj+OK09NUU0PBoHvXGSwIEAQBQHd3t5kdy7KKomSX9vaS1xtvbZVlOXNzFAQhhzEREXW/fAlA2LPHzJGiKCzLZpYkk0me58024oWjRDqdZlk2EAik02kz0xyPamoChw//c+UKPn9OJpOLLgVB0HX9wIEDiUQiu5GbNwEkfv8OhUIWjjJ9Gb0yE7x0FhIEwdBg1u5KGhsbo0NDyRcvlLdvub6+xXJZluPxuCAIdXV1qVRqqcK1a5iYACBJUjAYXCnPTE/+N7DoWNf1aDRqp91QKKTruqqqXH09YjE0N0PTMg1UVeV5vrGxceG3ouDNG1y/DkAURZ7ng8GgHUfWLBMgiqIoijbb1TRNFEVj8aC8HL29uHULd+9m2iiKkkgkNE2DpqG9HV1d2LrVeKSqajQa1XV9NQUY7aZSKYsJahCNRlOp1ELvDfbvR38/Hj3CjRuLZTzPcxynP3iAW7fQ0wOvN/ORLMuFv4RsASzLyrKsaZrFagZgTN9lAgCUliIWw7NnxjwxECoqEh8+IBJBaWlWI4qiAMg7WNbkuNDIssyyrHW7RtTiOC77gc+Hnh68eoWmJgB4/557/Tr99296166c7ciyHAqFrAcLgK7rjAm5b2SKooRCIZtRIhuPB7EYhobQ0ICGBpw9a2ErSRLP83lDnyiK+cNoJoIgGJHOjQAAJSVob0ckgr17sW+fta2xmm2GvpWY3okVRUmlUmZRgmXZdDq9LMZnMjaGkydx5w527kxpGrtjB8uyZo44jitkNZsKYFnW4iUYO0vuYTNSMufO4fJl6Hr050++qMg6tSHLcjqddrearXKjFqtZEASO43IImJ1FfT2qqxEMAkgMDaWmpsSDB3H8OCYnLXwpipI39DkWAEBVVbMokWPbnp/H+fPw+dDaahQEg0Ge56W+PlRV4dgxjI+bOTL2ZhcvIY8AnuclScoZJSRJEkUxEAgsrYTLlzEzg8ePjV+BQCCRSHR2dgJAWxvq6nD0qHEWyomqqpqmma4rM4xgxPO8Rajied7Y4FY+kiQJgCzLyYsXqbqapqeNI7GxB8ezbm0tLZ27dwPILv8PVVWN5T4+Pr5YKIoix3FmfVu6D0jmGenu7m4AZkf8eDwu19VlDorVfaCpCUAyFjPzZRyKM0us7wMF3MgWMXJS377ZtVdV8vtpeHgVXBd0pTTo6qLSUseJlnCYfD4aGCjUe6EC3r1zn5N6+rTAjJBBAQKMnJT5bM5PJEJeL/X2um/BvYDVyEkRLaQFKBJx3YArAT9+UFkZPXni2usy+vvJ53M9Fs4FGDmpO3fc+cvNwAD5fO6ydA4FTE/TkSOrlZNaxvAw+f308KHTek4EzM3RyZNr+AU2lSK/n9raHFVyIuDcOTp9em2/vX77RuXl1NJiv4ZtAS0tVFtLf/646ZYjRkepspI6Omya2/7L2dev2L4d27c7Oyq6Y3ISJSU2P1Ru/mduvdkUsN5sClhvihhmvbtQAAyDItBGVkBMETFgwGy498AwYMAQg38BTtJVzThWR3cAAAAASUVORK5CYII=";
+var gSl2DKey;
+var gSl3DKey;
+var gSlPostKey;
+var gSlCubeKey;
+var gSlVoxelKey;
+var gSlTerrainKey;
+var gNoneTex;
 export class CPalette {
     mMCI2D = new CMeshCreateInfo();
     mSL3D = null;
@@ -21,13 +28,23 @@ export class CPalette {
     async Load(_fw) {
         if (_fw.Ren() == null)
             return;
-        await _fw.Load().LoadSwitch(CPath.PHPC() + "artgine/z_file/none.png", CUtil.Base64ToArray(gNoneImg), new CLoaderOption());
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/3D.ts");
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/Voxel.ts");
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/2D.ts");
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/Post.ts");
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/Cube.ts");
-        await _fw.Load().Load(CPath.PHPC() + "artgine/z_file/Terrain.ts");
+        let upFolder = CPath.PHPC();
+        if (_fw.PF().mGitHub)
+            upFolder = "https://06fs4dix.github.io/Artgine/";
+        gNoneTex = upFolder + "artgine/z_file/none.png";
+        await _fw.Load().LoadSwitch(gNoneTex, CUtil.Base64ToArray(gNoneImg), new CLoaderOption());
+        gSl2DKey = upFolder + "artgine/z_file/2D.ts";
+        gSl3DKey = upFolder + "artgine/z_file/3D.ts";
+        gSlPostKey = upFolder + "artgine/z_file/Post.ts";
+        gSlCubeKey = upFolder + "artgine/z_file/Cube.ts";
+        gSlVoxelKey = upFolder + "artgine/z_file/Voxel.ts";
+        gSlTerrainKey = upFolder + "artgine/z_file/Terrain.ts";
+        await _fw.Load().Load(gSl3DKey);
+        await _fw.Load().Load(gSlVoxelKey);
+        await _fw.Load().Load(gSl2DKey);
+        await _fw.Load().Load(gSlPostKey);
+        await _fw.Load().Load(gSlCubeKey);
+        await _fw.Load().Load(gSlTerrainKey);
         this.mSL2D = _fw.Res().Find(this.Sl2DKey());
         this.mSL3D = _fw.Res().Find(this.Sl3DKey());
         this.mSLPost = _fw.Res().Find(this.SlPostKey());
@@ -61,17 +78,17 @@ export class CPalette {
     SlCube() { return this.mSLCube; }
     SlVoxel() { return this.mSLVoxel; }
     SlTerrain() { return this.mSLTerrain; }
-    Sl2DKey() { return CPath.PHPC() + "artgine/z_file/2D.ts"; }
-    Sl3DKey() { return CPath.PHPC() + "artgine/z_file/3D.ts"; }
-    SlPostKey() { return CPath.PHPC() + "artgine/z_file/Post.ts"; }
-    SlCubeKey() { return CPath.PHPC() + "artgine/z_file/Cube.ts"; }
-    SlVoxelKey() { return CPath.PHPC() + "artgine/z_file/Voxel.ts"; }
-    SlTerrainKey() { return CPath.PHPC() + "artgine/z_file/Terrain.ts"; }
+    Sl2DKey() { return gSl2DKey; }
+    Sl3DKey() { return gSl3DKey; }
+    SlPostKey() { return gSlPostKey; }
+    SlCubeKey() { return gSlCubeKey; }
+    SlVoxelKey() { return gSlVoxelKey; }
+    SlTerrainKey() { return gSlTerrainKey; }
     MCI2D() {
         return this.mMCI2D;
     }
     GetNoneTex() {
-        return CPath.PHPC() + "artgine/z_file/none.png";
+        return gNoneTex;
     }
     GetBlackTex() {
         return "Black.tex";
