49aee8dc374d5042bbbe49f3c4a3b5776c1631c0
06fs4dix <41887436+06fs4dix@users.noreply.github.com>
2025-10-09 22:41:55 +0900

    불 필요 파일 삭제

--- artgine/wasm/EM.h
+++
@@ -1,4 +0,0 @@
-#include <emscripten.h>
-#include <emscripten/bind.h>
-#include <emscripten/val.h>
-#include <emscripten/console.h>
--- artgine/wasm/Type.h
+++
@@ -1,7 +0,0 @@
-
-#define null nullptr
-#define var auto
-
-
-typedef int Int32;
-typedef unsigned int Uint32;
